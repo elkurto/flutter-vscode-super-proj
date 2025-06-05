7b057c5e32142afea37aa93a3966c900f72ce466
elkurto <kurt.buehner@gmail.com>
2025-06-05 12:57:28 -0400

    changed to Stateful Widget

--- aab_s16_097_custom_widget/lib/main.dart
+++ aab_s16_097_custom_widget/lib/main.dart
@@ -18,20 +18,50 @@ class MyApp extends StatelessWidget {
   }
 }
 
-class MyHomePage extends StatelessWidget {
+class MyHomePage extends StatefulWidget {
   final theme;
   const MyHomePage({this.theme, Key? key}) : super(key: key);
 
   @override
+  State<MyHomePage> createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+  int _count = 0;
+
+  @override
   Widget build(BuildContext context) {
     return Scaffold(
-      // fmtcomment
       appBar: AppBar(
-        backgroundColor: theme.primaryColor,
-        foregroundColor: theme.secondaryHeaderColor,
+        backgroundColor: widget.theme.primaryColor,
+        foregroundColor: widget.theme.secondaryHeaderColor,
         title: Text('AppBar in MyHomePage'), //
       ),
       backgroundColor: Colors.lightBlue,
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Text('Counter Worked', style: TextStyle(fontSize: 30)),
+            Text(
+              _count.toString(),
+              style: TextStyle(fontSize: 30, fontWeight: FontWeight.bold),
+            ),
+          ], //children
+        ),
+      ),
+      floatingActionButton: FloatingActionButton(
+        onPressed: () {
+          incrementCount();
+        },
+        child: Icon(Icons.add),
+      ),
     );
+  }
+
+  void incrementCount() {
+    setState(() {
+      _count += 1;
+    });
   }
 }
