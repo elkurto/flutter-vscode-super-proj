e021a0ab79c39e277d06b5da1e168d07f67c2ff1
elkurto <kurt.buehner@gmail.com>
2025-07-06 08:56:38 -0400

    reworking aba_paint_image with ui.Image

--- aba_paint_image/lib/app_paint_image.dart
+++ aba_paint_image/lib/app_paint_image.dart
@@ -1,3 +1,5 @@
+import 'dart:collection';
+import 'dart:ui' as ui;
 import 'package:flutter/material.dart';
 
 class AppPaintImageWidget extends StatefulWidget {
@@ -38,5 +40,49 @@ class _AppPaintImageWidgetState extends State<AppPaintImageWidget>
   @override
   Widget build(BuildContext buildContext) {
     return Text("@todo add canvas custompainter and render loaded image");
+  }
+}
+
+class Sprite {
+  ui.Image image;
+  double sx = 0.0;
+  double sy = 0.0;
+  double sw = 50.0;
+  double sh = 50.0;
+  double dx = 0.0;
+  double dy = 0.0;
+  double dw = 50.0;
+  double dh = 50.0;
+  final Paint paintBackground = Paint()..color = Color(0x00000000);
+
+  Sprite(
+    this.image,
+    this.sx,
+    this.sy,
+    this.sw,
+    this.sh,
+    this.dx,
+    this.dy,
+    this.dw,
+    this.dh,
+  );
+  void act(GameState gameState) {}
+  void draw(Canvas canvas, GameState gameState) {
+    Rect rectSrc = Rect.fromLTWH(sx, sy, sw, sh);
+    Rect rectDest = Rect.fromLTWH(dx, dy, dw, dh);
+    canvas.drawImageRect(image, rectSrc, rectDest, paintBackground);
+  }
+}
+
+class GameState {
+  Size? size;
+  int? prevEpochMillis;
+  int dt = 20;
+  final Map<String, Image> mapNameToImage = HashMap();
+  final List<Sprite> listSprite = [];
+
+  GameState() {
+    Image image = Image.asset("assets/boomerange.000.50x50.png");
+    mapNameToImage["boomerang"] = image;
   }
 }
