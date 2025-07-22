6bb0246574b416ad701b59b42cf7260d28172b16
elkurto <kurt.buehner@gmail.com>
2025-07-21 21:58:56 -0400

    abj_view3d_pts

---
+++ abj_view3d_pts/lib/app_paint_image.dart
@@ -0,0 +1,184 @@
+import 'dart:collection';
+
+import 'dart:math' as math;
+import 'package:flutter/material.dart';
+//import 'package:flutter/services.dart';
+import 'package:vector_math/vector_math_64.dart' as vector_math;
+import 'project.dart';
+
+class AppPaintImageWidget extends StatefulWidget {
+  const AppPaintImageWidget({super.key});
+
+  @override
+  State<AppPaintImageWidget> createState() => _AppPaintImageWidgetState();
+}
+
+class _AppPaintImageWidgetState extends State<AppPaintImageWidget>
+    with SingleTickerProviderStateMixin {
+  late final AnimationController _controller = AnimationController(
+    vsync: this,
+    upperBound: 100.0,
+  );
+  final Duration duration = const Duration(seconds: 20);
+  final GameState _gameState = GameState();
+
+  @override
+  void initState() {
+    super.initState();
+
+    _gameState.init();
+    _controller.duration = duration;
+    _controller.repeat();
+    _controller.addListener(_update);
+  }
+
+  void _update() {
+    setState(() => _gameState.act());
+  }
+
+  @override
+  void dispose() {
+    _controller.dispose();
+    super.dispose();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    _gameState.size ??= MediaQuery.of(context).size;
+    if (!_gameState.isLoaded()) {
+      return const Center(child: Text('loading...'));
+    }
+
+    return Container(
+      decoration: BoxDecoration(color: Colors.black),
+      child: GestureDetector(
+        onTapDown: (details) => _gameState.handleTap(details.localPosition),
+        child: CustomPaint(
+          painter: SpriteGamePainter(_gameState, _controller),
+          child: const SizedBox.expand(),
+        ),
+      ),
+    );
+  }
+}
+
+class SpriteGamePainter extends CustomPainter {
+  final GameState gameState;
+  final Animation<double> animation;
+
+  const SpriteGamePainter(this.gameState, this.animation)
+    : super(repaint: animation);
+
+  @override
+  void paint(Canvas canvas, Size size) {
+    gameState.size = size;
+    gameState.draw(canvas);
+  }
+
+  @override
+  bool shouldRepaint(covariant CustomPainter oldDelegate) {
+    return true;
+  }
+}
+
+class GameState {
+  Size? size;
+  int? prevEpochMillis;
+  int dt = 20;
+  int nImageLoaded = 0;
+  double theta = 0; // radian
+  double vtheta = math.pi / 10000; //  (pi radian / 10000 millisecond)
+  final List<vector_math.Vector3> listPointOrig = <vector_math.Vector3>[
+    vector_math.Vector3(0, 0, 0), // bottom corners of cube
+    vector_math.Vector3(1, 0, 0),
+    vector_math.Vector3(1, 0, 1),
+    vector_math.Vector3(0, 0, 1),
+    vector_math.Vector3(0, 1, 0), // top corners of cube
+    vector_math.Vector3(1, 1, 0),
+    vector_math.Vector3(1, 1, 1),
+    vector_math.Vector3(0, 1, 1),
+  ];
+  List<vector_math.Vector3> listPointRotated = List.filled(
+    8,
+    vector_math.Vector3(0, 0, 0),
+  );
+
+  static const _palette = [
+    Color(0xFFea1f25),
+    Color(0xFF83AAF3),
+    Color(0xFFf1ca00),
+    Color(0xFFecddbe),
+
+    Color(0xFF8C0307),
+    Color(0xFF043CA3),
+    Color(0xFF8B7506),
+    Color(0xFF858585),
+  ];
+
+  final _paint = Paint()..color = Color(0xFF66EE66);
+
+  init() {}
+
+  bool isLoaded() {
+    return (size != null);
+  }
+
+  void act() {
+    if (isLoaded()) {
+      //int nowMs = DateTime.now().millisecondsSinceEpoch;
+      //dt = nowMs - prevEpochMillis;
+      theta += vtheta * dt;
+      theta = theta % (2 * math.pi);
+    }
+  }
+
+  void draw(Canvas canvas) {
+    if (isLoaded()) {
+      var aspectRatio = size!.width / size!.height;
+
+      for (final (i, point) in listPointOrig.indexed) {
+        const pointSize = 5.0;
+        var screenPoint = project(
+          point,
+          theta, //animation.value * math.pi * 2,
+          aspectRatio,
+        );
+
+        var color = _palette[i % _palette.length];
+        _paint.color = color;
+
+        // Remaps coordinates from [-1, 1] to the [0, viewport].
+        var x = (1.0 + screenPoint.x) * size!.width / 2;
+        var y = (1.0 - screenPoint.y) * size!.height / 2;
+
+        canvas.drawCircle(Offset(x, y), pointSize, _paint);
+      }
+    }
+  }
+
+  void dispose() {}
+
+  void handleTap(Offset localOffset) {}
+}
+
+/*
+TODOS
+1. use drawAtlasRaw  (in lieu of drawImage) -- what is an RSTransform parameter 
+2. make GameState a const , so that SpriteGamePainter and CustomPainter can be const.
+3. add collision and inelastic collision
+4. mesh rendering 
+5. use Matrix4
+6. faux 3d rendering
+7. 2-3-4-tree in dart
+8. navigation multiscreen  - see https://github.com/flutter/website/blob/main/examples/ui/navigation/README
+9. adverts
+10. persistent local storage of game state 
+11. levels
+12. game-physics-sym
+13, sound controller
+
+    // @done make several sprites
+    // @done add touch events
+    // @todo add swipe events
+    // @todo render image to mesh or proceedural shader
+*/
---
+++ abj_view3d_pts/lib/project.dart
@@ -0,0 +1,43 @@
+import 'dart:math' as math;
+
+import 'package:vector_math/vector_math_64.dart';
+
+// Lifted from https://github.com/filiph/observable_flutter_cube/tree/main/lib/renderer
+// in an attempt to understand projection matrices.
+// Also go watch - Faking 3d graphics in flutter - https://www.youtube.com/watch?v=DmId-KOgPkg
+//  Filip Hracek - "Faking 3D in Flutter — pair programming with Craig Labenz"
+Vector2 project(Vector3 point, double rotation, double aspectRatio) {
+  final viewMatrix = makeViewMatrix(
+    Vector3(0.25, math.cos(rotation), math.sin(rotation)) * 2,
+    Vector3.all(0.5),
+    Vector3(1, 0, 0),
+  );
+
+  const near = 1.0;
+  const fov = 60.0;
+  const zoom = 1.0;
+  final double top = near * math.tan(radians(fov) / 2.0) / zoom;
+  final double bottom = -top;
+  final double right = top * aspectRatio;
+  final double left = -right;
+  const double far = 1000.0;
+
+  final projectionMatrix = makeFrustumMatrix(
+    left,
+    right,
+    bottom,
+    top,
+    near,
+    far,
+  );
+
+  final transformationMatrix = projectionMatrix * viewMatrix;
+
+  final projectiveCoords = Vector4(point.x, point.y, point.z, 1.0);
+  projectiveCoords.applyMatrix4(transformationMatrix);
+
+  var x = projectiveCoords.x / projectiveCoords.w;
+  var y = projectiveCoords.y / projectiveCoords.w;
+
+  return Vector2(x, y);
+}
