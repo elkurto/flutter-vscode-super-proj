4b0c5b9b11b3f249356356ccf61ec52553aa7ef6
elkurto <kurt.buehner@gmail.com>
2025-08-19 19:37:30 -0400

    in baa : added comments

--- baa_app_navigation/lib/main.dart
+++ baa_app_navigation/lib/main.dart
@@ -1,6 +1,17 @@
 import 'package:flutter/cupertino.dart';
 
 // source = https://docs.flutter.dev/cookbook/navigation/navigation-basics
+// this example uses Navigator.push(...) and Navigator.pop(...)
+// [other Navigator methods include](https://docs.flutter.dev/cookbook/navigation/navigation-basics#additional-navigation-methods) :
+// pushAndRemoveUntil: Adds a navigation route to the stack and then removes the most recent routes from the stack until a condition is met.
+// pushReplacement: Replaces the current route on the top of the stack with a new one.
+// replace: Replace a route on the stack with another route.
+// replaceRouteBelow: Replace the route below a specific route on the stack.
+// popUntil: Removes the most recent routes that were added to the stack of navigation routes until a condition is met.
+// removeRoute: Remove a specific route from the stack.
+// removeRouteBelow: Remove the route below a specific route on the stack.
+// restorablePush: Restore a route that was removed from the stack.
+
 void main() {
   runApp(const CupertinoApp(title: 'Navigation Basics', home: FirstRoute()));
 }
