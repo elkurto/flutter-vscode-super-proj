2805f6cca510469cb38c01b7d4c22a4d014fb8cc
elkurto <kurt.buehner@gmail.com>
2025-08-31 15:47:53 -0400

    abk_draw_input_controller : added InputControllerSingleton

--- abk_draw_input_controller/lib/input_controller.dart
+++ abk_draw_input_controller/lib/input_controller.dart
@@ -1,3 +1,21 @@
 // 1. Singleton
 // 2. listen for keyboard input ??? (need a widget) make this a widget with a stateful singleton
 // 3.
+
+class InputControllerSingleton {
+  // i have the state
+  bool firePrimaryPressed = false;
+  bool fireSecondaryPressed = false;
+  bool pausePressed = false;
+
+  // 1. private named constructor
+  //
+  InputControllerSingleton._internal();
+
+  // 2. static final field that refers to the singleton instance
+  static final InputControllerSingleton _instance =
+      InputControllerSingleton._internal();
+
+  // 3. static getter to allow clients to retreive instance
+  static InputControllerSingleton get instance => _instance;
+}
