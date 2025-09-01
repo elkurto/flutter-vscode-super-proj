7014c8224b87a110126d1a2075a5262f82f94707
elkurto <kurt.buehner@gmail.com>
2025-09-01 13:41:49 -0400

    abq_riverpod07_notifier_provider : created

---
+++ abq_riverpod07_notifier_provider/lib/screens/cart/cart_screen.dart
@@ -0,0 +1,63 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:riverpod_files/providers/products_provider.dart';
+
+/// see :branch:lesson-5 for updates A.7.x
+// A.7.1 extend ConsumerStatefulWidget instead of StatefulWidget
+//class CartScreen extends StatefulWidget { // old code
+class CartScreen extends ConsumerStatefulWidget {
+  // new code^^^
+  const CartScreen({super.key});
+
+  // A.7.3 create :ConsumerState<CartScreen>: instead of "State<CartScreen>"
+  @override
+  //State<CartScreen> createState() => _CartScreenState(); //old code
+  ConsumerState<CartScreen> createState() => _CartScreenState(); //new code
+}
+
+// A.7.2 extend ConsumerState<CartScreen> instead of State<CartScreen>
+// class _CartScreenState extends State<CartScreen> { // old code
+class _CartScreenState extends ConsumerState<CartScreen> {
+  // new code^^^
+  bool showCoupon = true;
+
+  @override
+  Widget build(BuildContext context) {
+    // A.7.4 watch the data from reducedProductsProvider (see products_provider.dart)
+    // Note: ref exposed as inherited field of "ConsumerState<CartScreen>"
+    final cartProducts = ref.watch(reducedProductsProvider);
+
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Your Cart'),
+        centerTitle: true,
+        // actions: [],
+      ),
+      body: Container(
+        padding: const EdgeInsets.all(30),
+        child: Column(
+          children: [
+            Column(
+              // A.7.5. use the watched_data from provider to display a item card.
+              children: cartProducts.map((product) {
+                return Container(
+                    padding: const EdgeInsets.only(top: 10, bottom: 10),
+                    child: Row(
+                      children: [
+                        Image.asset(product.image, width: 60, height: 60),
+                        const SizedBox(width: 10),
+                        Text('${product.title}...'),
+                        const Expanded(child: SizedBox()),
+                        Text('\$${product.price}'),
+                      ],
+                    ) // end-row
+                    ); // end-container
+              }).toList(),
+            ),
+            // output totals here
+          ],
+        ),
+      ),
+    );
+  }
+}
