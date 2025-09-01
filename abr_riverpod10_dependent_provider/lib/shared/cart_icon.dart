336b33f9b508b185040c5120d6cf0e5e6c5a21f9
elkurto <kurt.buehner@gmail.com>
2025-09-01 17:39:21 -0400

    abr_riverpod10_dependent_provider/lib/shared/cart_icon.dart added numberOfItemsInCart A.11 lesson-11

--- abr_riverpod10_dependent_provider/lib/shared/cart_icon.dart
+++ abr_riverpod10_dependent_provider/lib/shared/cart_icon.dart
@@ -1,11 +1,18 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:riverpod_files/screens/cart/cart_screen.dart';
+import 'package:riverpod_files/providers/cart_provider.dart';
 
-class CartIcon extends StatelessWidget {
+// A.11.1 convert Stateless widge to a ConsumerWidget
+class CartIcon extends ConsumerWidget {
   const CartIcon({super.key});
 
   @override
-  Widget build(BuildContext context) {
+  Widget build(BuildContext context, WidgetRef ref) {
+    // add :param:ref
+    // A.11.2 compute :int: numberOfItemsInCart from provider state
+    final numberOfItemsInCart = ref.watch(cartNotifierProvider).length;
+
     return Stack(
       children: [
         IconButton(
@@ -27,6 +34,10 @@ class CartIcon extends StatelessWidget {
               borderRadius: BorderRadius.circular(10),
               color: Colors.blueAccent,
             ),
+            // A.11.3 // use the value computed from Provider state.
+            child: Text(numberOfItemsInCart.toString(),
+                style: const TextStyle(color: Colors.white) // blah
+                ), // end
           ),
         ),
       ],
