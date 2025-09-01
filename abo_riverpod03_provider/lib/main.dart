179178962df259c2b9df145f00e308b573424408
elkurto <kurt.buehner@gmail.com>
2025-09-01 11:05:42 -0400

    abo_riverpod_provider : main.dart wrapped Widget in provider'

--- abo_riverpod03_provider/lib/main.dart
+++ abo_riverpod03_provider/lib/main.dart
@@ -1,8 +1,12 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:riverpod_files/screens/home/home_screen.dart';
 
 void main() {
-  runApp(const MyApp());
+  // A.2. ProviderScope wraps targetted :widget:"MyApp"
+  // runApp(const MyApp()); // old code
+  // now MyApp (and MyApp's descendants can consume data (from riverpod's Providers)
+  runApp(const ProviderScope(child: MyApp()));
 }
 
 class MyApp extends StatelessWidget {
