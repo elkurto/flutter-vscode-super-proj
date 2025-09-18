50c2cb9240e787597cefd44d18f1c8d62f45010b
elkurto <kurt.buehner@gmail.com>
2025-09-18 08:08:59 -0400

    abm07 : added gamescreenwidget

---
+++ abm07_mouse_key_listener/lib/gamescreenwidget.dart
@@ -0,0 +1,47 @@
+import 'package:abm07_mouse_key_listener/gamestate.dart';
+import 'package:flutter/material.dart';
+//import 'package:flutter/services.dart';
+
+class GameScreenWidget extends StatefulWidget {
+  const GameScreenWidget({super.key});
+}
+
+class _GameScreenWidgetState extends State<GameScreenWidget> with SingleTickerProviderStateMixin {
+  late final AnimationController _controller = AnimationController(vsync: this, upperBound: 100.0);
+  final Duration duration = const Duration(seconds: 20);
+  final GameState gameState = GameState.instance;
+
+  @override
+  void initState() {
+    super.initState();
+  }
+
+  void _update() {
+    setState(() => gameState.act());
+  }
+
+  @override
+  void dispose() {
+    _controller.dispose();
+    super.dispose();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    gameState.size ??= MediaQuery.of(context).size;
+    if (!gameState.isLoaded()) {
+      return const Center(child: Text('loading...'));
+    }
+
+    return Container(
+      decoration: BoxDecoration(color: Colors.black),
+      child: GestureDetector(
+        onTapDown: (details) => gameState.addSpriteAtLocalOffset(details.localPosition),
+        child: CustomPaint(
+          painter: SpriteGamePainter(gameState, _controller),
+          child: const SizedBox.expand(),
+        ),
+      ),
+    );
+  }
+}
