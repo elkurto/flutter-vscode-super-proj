1f6af273d8d4cfe8f6ec8f6da14a455e517d3eda
elkurto <kurt.buehner@gmail.com>
2025-09-24 08:11:00 -0400

    abm08 : cleanup

--- abm08_mouse_key_listener/lib/main.dart
+++ abm08_mouse_key_listener/lib/main.dart
@@ -1,5 +1,4 @@
 import 'package:abm08_mouse_key_listener/gamescreenwidget.dart' show GameScreenWidget;
-import 'package:abm08_mouse_key_listener/gamestate.dart';
 import 'package:flutter/material.dart';
 
 void main() {
@@ -22,45 +21,6 @@ class MyApp extends StatelessWidget {
         appBar: AppBar(title: Text(appTitle)),
         backgroundColor: Colors.red,
         body: Center(child: const GameScreenWidget()),
-      ),
-    );
-  }
-}
-
-class MouseTrackerExample extends StatefulWidget {
-  const MouseTrackerExample({super.key});
-
-  @override
-  MouseTrackerExampleState createState() => MouseTrackerExampleState();
-}
-
-class MouseTrackerExampleState extends State<MouseTrackerExample> {
-  Offset _mousePosition = Offset.zero;
-  final GameState gameState = GameState.instance;
-
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(title: Text('Mouse Tracking Example')),
-      body: Center(
-        child: MouseRegion(
-          onHover: (event) {
-            setState(() {
-              _mousePosition = event.localPosition;
-            });
-          },
-          // create a 300x200 window and track mouse only in this window.
-          child: Container(
-            width: 300,
-            height: 200,
-            color: Colors.blueGrey[100],
-            alignment: Alignment.center,
-            child: Text(
-              'Mouse Position: (${_mousePosition.dx.toStringAsFixed(2)}, ${_mousePosition.dy.toStringAsFixed(2)})',
-              style: TextStyle(fontSize: 16),
-            ),
-          ),
-        ),
       ),
     );
   }
