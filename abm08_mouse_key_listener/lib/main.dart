a45645d12c3173faefaab023e640d8a86fcbc729
elkurto <kurt.buehner@gmail.com>
2025-09-24 18:50:41 -0400

    abm08 : OuterRouter

--- abm08_mouse_key_listener/lib/main.dart
+++ abm08_mouse_key_listener/lib/main.dart
@@ -1,8 +1,28 @@
 import 'package:abm08_mouse_key_listener/gamescreenwidget.dart' show GameScreenWidget;
 import 'package:flutter/material.dart';
+import 'package:go_router/go_router.dart';
 
 void main() {
-  runApp(const MyApp(appTitle: 'abm08_mouse_key_listener'));
+  //runApp(const MyApp(appTitle: 'abm08_mouse_key_listener'));
+  runApp(const OuterRouterAppWidget());
+}
+
+class OuterRouterAppWidget extends StatelessWidget {
+  OuterRouterAppWidget({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp.router(
+      routerConfig: _router,
+      title: 'abm08_mouse_key_listener',
+      debugShowCheckedModeBanner: false,
+    )
+  }
+
+  late final GoRouter _router =GoRouter(
+    debugLogDiagnostics: true,
+
+  )
 }
 
 class MyApp extends StatelessWidget {
