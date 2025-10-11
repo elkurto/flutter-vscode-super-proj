873693758284162eb25edad1410ebc1c998e20b9
elkurto <kurt.buehner@gmail.com>
2025-10-11 14:17:35 -0400

    nna11_column_widget : fixed styles

--- nna11_column_widget/lib/main.dart
+++ nna11_column_widget/lib/main.dart
@@ -2,7 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:nna11_column_widget/home.dart';
 
 void main() {
-  runApp(const MaterialApp(home: Sandbox()));
+  runApp(const MaterialApp(home: Sandbox(), debugShowCheckedModeBanner: false));
 }
 
 // sandbox
@@ -12,15 +12,40 @@ class Sandbox extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(title: const Text('Sandbox'), backgroundColor: Colors.grey),
-      body: Column(
-        crossAxisAlignment: CrossAxisAlignment.stretch,
-        mainAxisAlignment: MainAxisAlignment.center,
-        children: [
-          Container(width: 100, color: Colors.red, child: const Text('one')),
-          Container(width: 200, color: Colors.green, child: const Text('two')),
-          Container(width: 300, color: Colors.blue, child: const Text('three')),
-        ],
+      appBar: AppBar(
+        title: const Text(
+          'Sandbox',
+          style: TextStyle(color: Colors.white, fontWeight: FontWeight.bold),
+        ),
+        backgroundColor: Colors.grey,
+        centerTitle: true,
+      ),
+      body: Container(
+        color: Colors.black,
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.stretch,
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Container(
+              width: 100,
+              color: Colors.red[200],
+              padding: const EdgeInsets.all(20),
+              child: const Text('Red'),
+            ),
+            Container(
+              width: 200,
+              color: Colors.green[200],
+              padding: const EdgeInsets.all(20),
+              child: const Text('Green'),
+            ),
+            Container(
+              width: 300,
+              color: Colors.blue[200],
+              padding: const EdgeInsets.all(20),
+              child: const Text('Blue'),
+            ),
+          ],
+        ),
       ),
     );
   }
