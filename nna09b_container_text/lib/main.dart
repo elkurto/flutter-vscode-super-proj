718e02674c1ed397731b160a08fc27fcac233cce
elkurto <kurt.buehner@gmail.com>
2025-10-11 13:45:02 -0400

    nna09b_container_text : refacted :widget:Home to home.dart

--- nna09b_container_text/lib/main.dart
+++ nna09b_container_text/lib/main.dart
@@ -1,39 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:nna09b_container_text/home.dart';
 
 void main() {
   runApp(MaterialApp(home: Home()));
-}
-
-class Home extends StatelessWidget {
-  const Home({super.key});
-
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: const Text(
-          'My Styled Widget Title',
-          style: TextStyle(color: Colors.white, fontWeight: FontWeight.bold),
-        ),
-        backgroundColor: Colors.brown[700],
-        centerTitle: true,
-      ),
-      body: Container(
-        color: Colors.orange,
-        // width: 200,
-        // height: 100,
-        padding: const EdgeInsets.all(20),
-        margin: const EdgeInsets.fromLTRB(10, 40, 0, 0),
-        child: const Text(
-          'Rudy Ray Moore! \nSignified Monkey',
-          style: TextStyle(
-            fontSize: 18,
-            letterSpacing: 4,
-            decoration: TextDecoration.underline,
-            fontStyle: FontStyle.italic,
-          ),
-        ),
-      ),
-    );
-  }
 }
