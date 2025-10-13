3faa3db100f0b862a0bc397c9b25b892e8561f69
elkurto <kurt.buehner@gmail.com>
2025-10-13 07:31:24 -0400

    nna21_theme_custom : created and commented

---
+++ nna21_theme_custom/lib/screens/home/home.dart
@@ -0,0 +1,36 @@
+import 'package:flutter/material.dart';
+
+class Home extends StatefulWidget {
+  const Home({super.key});
+
+  @override
+  State<Home> createState() => _HomeState();
+}
+
+class _HomeState extends State<Home> {
+  // define the home screen -- just a landing page.
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(title: const Text('Your Characters'), centerTitle: true),
+      body: Container(
+        padding: const EdgeInsets.all(16),
+        child: Column(
+          children: [
+            const Text('character list'),
+            Text('character list', style: Theme.of(context).textTheme.headlineMedium),
+            Text('character list', style: Theme.of(context).textTheme.titleMedium),
+
+            FilledButton(
+              onPressed: () {
+                // navigate to the create screen
+              },
+              child: const Text('Create New'),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
