0d1a97e7da1cc573601983370d7dd67729e154a2
elkurto <kurt.buehner@gmail.com>
2025-10-18 10:34:21 -0400

    nna22_theme_shared : created - uses custom widget that use custom theme

---
+++ nna22_theme_shared/lib/screens/home/home.dart
@@ -0,0 +1,35 @@
+import 'package:flutter/material.dart';
+import 'package:nna22_theme_shared/shared/styled_text.dart';
+
+class Home extends StatefulWidget {
+  const Home({super.key});
+
+  @override
+  State<Home> createState() => _HomeState();
+}
+
+class _HomeState extends State<Home> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(title: const StyledTitle('Your Characters'), centerTitle: true),
+      body: Container(
+        padding: const EdgeInsets.all(16),
+        child: Column(
+          children: [
+            const StyledTitle('title'),
+            const StyledHeading('heading'),
+            const StyledText('text'),
+
+            FilledButton(
+              onPressed: () {
+                // navigate to the create screen
+              },
+              child: const Text('Create New'),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
