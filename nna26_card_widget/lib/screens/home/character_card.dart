2e9ba03d916bd1e2ccd1400529617d56a121bfae
elkurto <kurt.buehner@gmail.com>
2025-10-27 06:02:39 -0400

    nna26_card_widget : lesson-27 : added card_widget/lib/screens/home/character_card.dart

--- nna26_card_widget/lib/screens/home/character_card.dart
+++ nna26_card_widget/lib/screens/home/character_card.dart
@@ -17,7 +17,17 @@ class CharacterCard extends StatelessWidget {
       child: Padding(
         padding: const EdgeInsets.symmetric(horizontal: 16, vertical: 8),
 
-        child: Row(children: [Text(character)]),
+        child: Row(
+          children: [
+            Text(character),
+            // :widget:Expanded pushes icon to far right (layout/right-justify)
+            const Expanded(child: SizedBox()),
+            IconButton(
+              onPressed: () {},
+              icon: Icon(Icons.arrow_forward, color: AppColors.textColor),
+            ),
+          ],
+        ),
       ),
     );
   }
