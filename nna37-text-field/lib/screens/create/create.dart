7828f5f1087d244dd50fa485d4cbf30f23e318ad
elkurto <kurt.buehner@gmail.com>
2025-10-28 07:52:13 -0400

    nna37_text_field : created

---
+++ nna37-text-field/lib/screens/create/create.dart
@@ -0,0 +1,53 @@
+import 'package:flutter/material.dart';
+import 'package:nna37_text_field/shared/styled_text.dart';
+import 'package:nna37_text_field/theme.dart';
+import 'package:google_fonts/google_fonts.dart';
+
+class Create extends StatefulWidget {
+  const Create({super.key});
+
+  @override
+  State<Create> createState() => _CreateState();
+}
+
+class _CreateState extends State<Create> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(title: const StyledTitle('Character Creation')),
+      body: Container(
+        padding: const EdgeInsets.symmetric(vertical: 30, horizontal: 20),
+        child: Column(
+          children: [
+            // welcome message
+            Center(child: Icon(Icons.code, color: AppColors.primaryColor)),
+            const Center(child: StyledHeading('Welcome, new player.')),
+            const Center(child: StyledText('Create a name & slogan for your character.')),
+            const SizedBox(height: 30),
+
+            // input for name & slogan
+            TextField(
+              cursorColor: AppColors.textColor,
+              decoration: const InputDecoration(
+                prefixIcon: Icon(Icons.person_2),
+                label: StyledText('Character name'),
+              ),
+              style: GoogleFonts.kanit(textStyle: Theme.of(context).textTheme.bodyMedium),
+            ),
+            const SizedBox(height: 20),
+            TextField(
+              cursorColor: AppColors.textColor,
+              decoration: const InputDecoration(
+                prefixIcon: Icon(Icons.chat),
+                label: StyledText('Character slogan'),
+              ),
+              style: GoogleFonts.kanit(textStyle: Theme.of(context).textTheme.bodyMedium),
+            ),
+            const SizedBox(height: 30),
+            TextField(),
+          ],
+        ),
+      ),
+    );
+  }
+}
