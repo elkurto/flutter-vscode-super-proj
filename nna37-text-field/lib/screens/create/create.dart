e23e99f3baa3458b0cc30d419f32b85d621232ad
elkurto <kurt.buehner@gmail.com>
2025-10-29 08:02:06 -0400

    nna37-text-field : L39 and L38 - add TextController and Submit button to create.dart

--- nna37-text-field/lib/screens/create/create.dart
+++ nna37-text-field/lib/screens/create/create.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:nna37_text_field/shared/styled_button.dart';
 import 'package:nna37_text_field/shared/styled_text.dart';
 import 'package:nna37_text_field/theme.dart';
 import 'package:google_fonts/google_fonts.dart';
@@ -11,11 +12,41 @@ class Create extends StatefulWidget {
 }
 
 class _CreateState extends State<Create> {
+  // Either use (text fields and onChange) or (TextEditingControllers)
+  // Here we use TextEditingController
+  final _nameController = TextEditingController();
+  final _sloganController = TextEditingController();
+
+  @override
+  void dispose() {
+    // explicitly invoke TextEditingController::dispose()
+    _nameController.dispose();
+    _sloganController.dispose();
+    super.dispose();
+  }
+
+  // a dummy submit handler --
+  // the goal of submitHandlers; update the data-model
+  void handleSubmit() {
+    // validations
+    if (_nameController.text.trim().isEmpty) {
+      print('name must not be empty');
+      return;
+    }
+    if (_sloganController.text.trim().isEmpty) {
+      print('slogan must not be empty');
+      return;
+    }
+    print(_nameController.text);
+    print(_sloganController.text);
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(title: const StyledTitle('Character Creation')),
       body: Container(
+        // edge padding
         padding: const EdgeInsets.symmetric(vertical: 30, horizontal: 20),
         child: Column(
           children: [
@@ -25,8 +56,9 @@ class _CreateState extends State<Create> {
             const Center(child: StyledText('Create a name & slogan for your character.')),
             const SizedBox(height: 30),
 
-            // input for name & slogan
+            // input widget for name
             TextField(
+              controller: _nameController, // assign controller to textField
               cursorColor: AppColors.textColor,
               decoration: const InputDecoration(
                 prefixIcon: Icon(Icons.person_2),
@@ -34,8 +66,11 @@ class _CreateState extends State<Create> {
               ),
               style: GoogleFonts.kanit(textStyle: Theme.of(context).textTheme.bodyMedium),
             ),
+            // vertical space
             const SizedBox(height: 20),
+            // input widget for slogan
             TextField(
+              controller: _sloganController, // assign controller to textField
               cursorColor: AppColors.textColor,
               decoration: const InputDecoration(
                 prefixIcon: Icon(Icons.chat),
@@ -44,7 +79,14 @@ class _CreateState extends State<Create> {
               style: GoogleFonts.kanit(textStyle: Theme.of(context).textTheme.bodyMedium),
             ),
             const SizedBox(height: 30),
-            TextField(),
+
+            // submit button w/ onSubmit
+            Center(
+              child: StyledButton(
+                onPressed: handleSubmit,
+                child: const StyledHeading('Create Character'),
+              ),
+            ),
           ],
         ),
       ),
